7e73d2d10413409f1ec876baec0b36ea22b2fcf4
Abraão Santos <115851871+TechAbraao@users.noreply.github.com>
2024-08-12 15:19:00 -0300

    Add files via upload

---
+++ livros-estudos/caderno-de-algoritmos-instituto-federal/algoritmos-python/10-adicao-numeros-inteiros.py
@@ -0,0 +1,6 @@
+#
+primeiroNumero = int(input("Digite o primeiro número: "))
+segundoNumero = int(input("Digite o segundo número: "))
+#
+print(f"{primeiroNumero} + {segundoNumero} = {primeiroNumero + segundoNumero}")
+
---
+++ livros-estudos/caderno-de-algoritmos-instituto-federal/algoritmos-python/9-ler-concatenar.py
@@ -0,0 +1,5 @@
+#
+nome = str(input("Digite o primeiro nome: "))
+sobrenome = str(input("Digite o sobrenome: "))
+# Apresentação do nome completo
+print("Seu nome completo é:", nome + " " + sobrenome)
