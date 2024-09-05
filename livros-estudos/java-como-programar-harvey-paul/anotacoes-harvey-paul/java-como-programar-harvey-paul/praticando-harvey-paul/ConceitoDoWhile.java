74bcc854aaadfa62b7c77cc571d791cca474b295
Abraão V. S. Santos <115851871+TechAbraao@users.noreply.github.com>
2024-09-05 10:35:00 -0300

    commit

---
+++ livros-estudos/java-como-programar-harvey-paul/anotacoes-harvey-paul/java-como-programar-harvey-paul/praticando-harvey-paul/ConceitoDoWhile.java
@@ -0,0 +1,12 @@
+package com.exercicios.fatec;
+
+public class ConceitoDoWhile {
+	public static void main(String[] args) {
+		int count = 0;
+		
+		do {
+			System.out.println("Hello World!");
+			count++;
+		} while (count <= 5);
+	}
+}
